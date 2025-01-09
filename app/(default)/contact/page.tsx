15085850d79d816e5b51e179b33614d2947da5c8
Joshua Joseph <75155367+Joshua-Joseph1@users.noreply.github.com>
2025-01-09 16:40:40 +0200

    Add Contact Page Component and Update Metadata for Cedar Digital

---
+++ app/(default)/contact/page.tsx
@@ -0,0 +1,23 @@
+export const metadata = {
+    title: 'Cedar Digital',
+    description: 'Cedar Digital was born as a consultancy dedicated to building and scaling businesses, providing innovative strategies and solutions to help brands thrive in a competitive landscape.',
+  }
+  
+  import Hero from '@/components/hero'
+  import Clients from '@/components/clients'
+  import Features from '@/components/features'
+  import TestimonialsCarousel from '@/components/testimonials-carousel'
+  import Testimonials from '@/components/testimonials'
+  import Cta from '@/components/cta'
+  import Story from '@/components/story'
+  import Contact from '@/components/contact'
+  
+  export default function Home() {
+    return (
+      <>
+        <Contact />
+
+      </>
+    )
+  }
+  
---
+++ components/contact.tsx
@@ -0,0 +1,51 @@
+import Image from 'next/image'
+import Particles from './particles'
+import Illustration from '@/public/images/glow-bottom.svg'
+
+export default function Hero() {
+  return (
+    <section>
+      <div className="relative w-full px-4 sm:px-6">
+        {/* Particles animation */}
+        <Particles className="absolute inset-0 -z-10" />
+
+        {/* Illustration */}
+        <div
+          className="absolute inset-0 -z-10 -mx-28 rounded-b-[3rem] pointer-events-none overflow-hidden"
+          aria-hidden="true"
+        >
+          <div className="absolute left-1/2 -translate-x-1/2 bottom-0 -z-10">
+            <Image
+              src={Illustration}
+              className="max-w-none"
+              width={2146}
+              priority
+              alt="Hero Illustration"
+            />
+          </div>
+        </div>
+
+        <div className="pt-32 pb-16 md:pt-52 md:pb-32">
+          {/* Hero content */}
+          <div className="max-w-3xl mx-auto text-center">
+            <h2 className="text-3xl font-bold mb-6" data-aos="fade-down">Schedule a Call</h2>
+            <div className="mb-17" data-aos="fade-down">
+              {/* Calendar iframe */}
+              <iframe
+                src="https://api.leadconnectorhq.com/widget/booking/VFq5bKrRGXDIZ5Km2Xcj"
+                style={{ width: '100%', height: '1500px', border: 'none', overflow: 'hidden' }}
+                scrolling="no"
+                id="VFq5bKrRGXDIZ5Km2Xcj_1736433218173"
+              ></iframe>
+              <br />
+              <script
+                src="https://link.msgsndr.com/js/form_embed.js"
+                type="text/javascript"
+              ></script>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
